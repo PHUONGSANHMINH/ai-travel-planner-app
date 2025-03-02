fe1ed51e8c53ce6da7cbd4dedec7995bc35fe0e9
nhikhanhh <khanhnhi931819@gmail.com>
2025-03-02 22:16:33 +0700

    51:26

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,21 +1,46 @@
 import { View, Text, Image, StyleSheet} from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/Colors'
+import { useRouter } from 'expo-router'
+import { TouchableOpacity } from 'react-native';
 
 export default function Login() {
+
+  const router = useRouter();
   return (
     <View>
       <Image source={require('./../assets/images/login.jpg')}
       style={{
         width:'100%',
-        height:500
+        height:520
       }}
       />
       <View style={styles.container}>
         <Text style={{
-          fontSize: 25,
-          fontFamily: 'outfit-bold'
+          fontSize: 30,
+          fontFamily: 'outfit-bold',
+          textAlign: 'center',
+          marginTop: 10
         }}> AI Travel Planner </Text>
+
+        <Text style={{
+          fontFamily: 'outfit',
+          fontSize: 17,
+          textAlign: 'center',
+          color: Colors.GRAY,
+          marginTop: 20
+        }}>Discover your next adventure effortlessly. Personalized itineraries at your fingertips. Travel smarter with AI-driven insights.</Text>
+        
+        <TouchableOpacity style={styles.button}
+          onPress={()=>router.push('auth/sign-in')}
+        >
+          <Text style={{color: Colors.WHITE,
+            textAlign: 'center',
+            fontFamily: 'outfit',
+            fontSize: 17
+          }}>Get Started</Text>
+        </TouchableOpacity>
+      
       </View>
     </View>
   )
@@ -25,6 +50,15 @@ const styles = StyleSheet.create({
   container:{
     backgroundColor:Colors.WHITE,
     marginTop: -20,
-    height:'100%'
+    borderTopLeftRadius:20,
+    borderTopRightRadius:20,
+    height:'100%',
+    padding:25
+  },
+  button:{
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 99,
+    marginTop: '20%'
   }
 })
