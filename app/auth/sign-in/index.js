5c89aeff59110fb6603dcc76320d64938e99cff3
nhikhanhh <khanhnhi931819@gmail.com>
2025-03-05 06:31:01 +0700

    1:21:18

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -28,17 +28,21 @@ export default function SignIn() {
         ToastAndroid.show("Please enter Email and Password", ToastAndroid.LONG);
         return;
       }
-    signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage)
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+      })
+      .catch((error) => {
+     const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage,errorCode)
+    if (errorCode=='auth/invalid-credential')
+    {
+      ToastAndroid.show("Invalid credentials", ToastAndroid.LONG)
+    }
   });
 
   }
