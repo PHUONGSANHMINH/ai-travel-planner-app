29404f3c65ed4db2e7a6ed2aed22f6e007335fad
nhikhanhh <khanhnhi931819@gmail.com>
2025-03-11 07:38:55 +0700

    ..

--- app/create-trip/search-place.jsx
+++ app/create-trip/search-place.jsx
@@ -1,10 +1,39 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from 'expo-router'
+import { Colors } from './../../constants/Colors'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import 'react-native-get-random-values';
 
 export default function SearchPlace() {
+
+  const navigation = useNavigation();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShown: true,
+      headerTransparent: true,
+      headerTitle: 'Search'
+    })
+  },[])
   return (
-    <View>
-      <Text> SearchPlace</Text>
+    <View style={{
+       padding: 25,
+       paddingTop: 50,
+       backgroundColor:Colors.WHITE,
+       height: '100%', 
+    }}>
+    <GooglePlacesAutocomplete
+      placeholder='Search'
+      onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+        console.log(data, details);
+      }}
+      query={{
+        key: process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY,
+        language: 'en',
+      }}
+    />
     </View>
   )
 }
