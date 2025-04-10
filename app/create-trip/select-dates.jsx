ef6a525aa019c878d316ea615467835af72c997b
nhikhanhh <khanhnhi931819@gmail.com>
2025-04-10 22:47:27 +0700

    2:31:29

---
+++ app/create-trip/select-dates.jsx
@@ -0,0 +1,53 @@
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation } from 'expo-router'
+import { Colors } from '@/constants/Colors';
+import CalendarPicker from "react-native-calendar-picker";
+
+export default function SelectDates() {
+
+    const navigation = useNavigation();
+    
+    useEffect(()=>{
+        navigation.setOptions({
+            headerShown: true,
+            headerTransparent: true,
+            headerTitle: ''
+        })
+    },[])
+
+  return (
+    <View
+    style={{
+        padding: 25,
+        paddingTop: 75,
+        backgroundColor: Colors.WHITE,
+        height: '100%'
+    }}
+    >
+      <Text style={{
+        fontFamily: 'outfit-bold',
+        fontSize: 35,
+        marginTop: 20,
+      }}>Travel Dates</Text>
+      <View style={{
+        marginTop: 30
+      }}>
+      <CalendarPicker 
+      onDateChange={this.onDateChange} 
+      allowRangeSelection={true}
+      minDate={new Date()}
+      maxRangeDuration={10}
+      selectedRangeStyle={{
+        backgroundColor:Colors.PRIMARY
+      }}
+      selectedDayTextStyle={{
+        color: Colors.WHITE
+      }}
+      />
+      </View>
+
+      
+    </View>
+  )
+}
