6daad1026dcfc62d52505b9a669b1a4673cd05fb
nhikhanhh <khanhnhi931819@gmail.com>
2025-04-17 21:00:13 +0700

    ..

--- app/create-trip/generate-trip.jsx
+++ app/create-trip/generate-trip.jsx
@@ -1,46 +1,82 @@
 import { View, Text, Image } from 'react-native'
-import React, { useContext } from 'react'
-import { Colors } from '@/constants/Colors'
-import { CreateTripContext } from '@/context/CreateTripContext'
+import React, { useContext, useEffect, useState } from 'react'
+import { CreateTripContext } from '../../context/CreateTripContext';
+import { AI_PROMPT } from '../../constants/Options';
+import { chatSession } from '../../configs/AiModal';
+import { useRouter } from 'expo-router';
+import { auth, db } from './../../configs/FirebaseConfig'
+import { doc, setDoc } from 'firebase/firestore';
+
 
 export default function GenerateTrip() {
+  const { tripData, setTripData } = useContext(CreateTripContext);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const user = auth.currentUser;
 
-    const {tripData, setTripData}=useContext(CreateTripContext);
+  useEffect(() => {
+    GenerateAiTrip()
+  }, [])
+  
+  const GenerateAiTrip = async () => {
+    setLoading(true);
+    const FINAL_PROMPT = AI_PROMPT.replace('{location}', tripData?.locationInfo?.name)
+      .replace('{totalDays}', tripData.totalNoOfDays)
+      .replace('{totalNight}', tripData.totalNoOfDays - 1)
+      .replace('{traveler}', tripData.traveler?.title)
+      .replace('{budget}', tripData.budget)
+      .replace('{totalDays}', tripData.totalNoOfDays)
+      .replace('{totalNight}', tripData.totalNoOfDays - 1)
 
+    console.log(FINAL_PROMPT);
+    const result = await chatSession.sendMessage(FINAL_PROMPT);
+    console.log(result.response.text());
+    const tripResp = JSON.parse(result.response.text());
+    setLoading(false)
+    const docId = (Date.now()).toString();
+    const result_ = await setDoc(doc(db, 'UserTrips', docId), {
+      userEmail: user.email,
+      tripPlan: tripResp,//ai result
+      tripData: JSON.stringify(tripData),
+      //user selec data
+      docId: docId
+    })
+
+    router.push('(tabs)/mytrip')
+
+
+  }
   return (
     <View style={{
-        padding: 25,
-        paddingTop: 75,
-        backgroundColor: Colors.WHITE,
-        height: '100%'
+      padding: 25,
+      paddingTop: 75,
+      backgroundColor: '#fff',
+      height: '100%'
     }}>
       <Text style={{
         fontFamily: 'outfit-bold',
-        fontSize: 35,
-        textAlign: 'center',
-      }}>Please wait...</Text>
-       <Text style={{
+        fontSize: 30,
+        textAlign: 'center'
+      }}>Please Wait.....</Text>
+      <Text style={{
         fontFamily: 'outfit-medium',
-        fontSize: 20,
+        fontSize: 18,
         textAlign: 'center',
         marginTop: 40
       }}>We are working to generate your dream trip</Text>
 
-      <Image source = {require('./../../assets/images/plane.gif')}
-      style={{
-        width: '100%',
-        height: 200,
-        objectFit: 'contain'
-      }}
-      />
-
+      <Image source={require('./../../assets/images/plane.gif')}
+        style={{
+          width: "100%",
+          objectFit: 'contain',
+          height: 200
+        }} />
       <Text style={{
         fontFamily: 'outfit',
-        color: Colors.GRAY,
-        fontSize: 20,
-        textAlign: 'center',
-
-      }}>Do not back</Text>
+        color: '#808080',
+        fontSize: 18,
+        textAlign: 'center'
+      }}>Do not Go Back</Text>
     </View>
   )
 }
